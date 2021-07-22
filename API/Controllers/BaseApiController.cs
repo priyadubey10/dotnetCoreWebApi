046755ec0ac2d23fc5f55f81afa18d518b3fa7c8
Priya Dubey <priya.dubey@pwc.com>
2021-07-22 14:11:25 +0530

    Reactivities : Created webapi using dotnet entity framework

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
